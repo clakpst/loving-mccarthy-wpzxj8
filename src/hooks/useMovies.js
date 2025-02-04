7c38b7d66af912728e4c3b445e2de11c8cbf9064
clakpst3 <117895519+clakpst@users.noreply.github.com>
2025-02-04 16:56:27 +0000

    aaa

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,27 +1,13 @@
-import withResults from "../mocks/with-results.json";
-import withoutResults from "../mocks/no-results.json";
+import { searchMovies } from "../services/movies";
 import { useState } from "react";
 
 export function useMovies({ search }) {
-  const [responseMovies, setResponseMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  const movies = responseMovies.Search;
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
-
-  const getMovies = () => {
-    if (search) {
-      //setResponseMovies(withResults);
-      fetch(`https://www.omdbapi.com/?apikey=4287ad07&s=${search}`)
-        .then((res) => res.json())
-        .then((json) => setResponseMovies(json));
-    } else {
-      setResponseMovies(withoutResults);
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search });
+    setMovies(newMovies);
   };
-  return { movies: mappedMovies, getMovies };
+
+  return { movies, getMovies };
 }
